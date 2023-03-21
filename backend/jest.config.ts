74df12646ffd983d3ac4ef132003770e43c30d8b
verissimo santos <jose.neto@minu.co>
2023-03-20 21:31:34 -0300

    chore: removed module path

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,14 +1,7 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-
-import { compilerOptions } from './tsconfig.json';
-
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
   modulePaths: ['node_modules', '<rootDir>'],
   testMatch: ['**/?(*.)+(spec|test).ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/src/',
-  }),
 };
